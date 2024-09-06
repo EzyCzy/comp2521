fabdaedd9acf7be36f2353ae909d0117217aeb86
EzyCzy <82717285+EzyCzy@users.noreply.github.com>
2024-09-06 17:17:31 +1000

    Create bubble_sort.c

---
+++ note/week_2/bubble_sort.c
@@ -0,0 +1,12 @@
+void bubbleSort(Item items[], int lo, int hi){
+  for (int i = hi; i > lo; i--){ // in case we're not sorting the entire array (just some part)
+    bool swapped = false;
+    for (int j = lo; j < i; j++){
+      if(items[j] > items[j+1]){
+        swap(items, j, j+1);
+        swapped = true;
+      }
+    }
+    if(!swapped) break;
+  }
+}
