74f19367d2dc08db1b2a9b75e0912932adb36a1c
EzyCzy <82717285+EzyCzy@users.noreply.github.com>
2024-09-10 22:44:30 +1000

    Update reverse_linked_list_II.c

--- leetcode/reverse_linked_list_II.c
+++ leetcode/reverse_linked_list_II.c
@@ -1,1 +1,41 @@
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseBetween(struct ListNode* head, int left, int right) {
+
+  // If empty list or no reverse needed
+  if (head == NULL || left == right) return head;
+
+  // creating dummy node to avoid edge cases
+  struct ListNode *dummy = malloc(sizeof(*dummy));
+  dummy->next = head;
+
+  struct ListNode *prev = dummy;
+
+  //traverse so current is at the left node
+  for(int i = 0; i < left - 1; i++){
+    prev = prev->next;
+  }
+
+  struct ListNode *cur = prev->next;
+  struct ListNode *follow = NULL;
+
+  //traverse so follow is at the right node
+  for(int i = 0; i < right - left + 1; i++){
+    struct ListNode *tmp = cur->next;
+    cur->next = follow;
+    follow = cur;
+    cur = tmp;
+  }
+
+  //connect the nodes
+  prev->next->next = cur;
+  prev->next = follow;
+
+  return dummy->next;
+}
