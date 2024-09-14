9b7b0879527f4e430e89a6c83f58a476954f22da
EzyCzy <mirulh406@gmail.com>
2024-09-14 21:54:23 +1000

    Added queue template

---
+++ leetcode/queue/queue_template.c
@@ -0,0 +1,84 @@
+struct queue {
+    struct node *head;
+    struct node *tail;
+    int size;
+}
+
+struct node {
+    int val;
+    struct node *next;
+}
+
+Queue QueueNew(void){
+    Queue q = malloc(sizeof(struct queue));
+
+    q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
+
+    return q;
+}
+
+void QueueFree(Queue q){
+    struct node * cur = q->head;
+    while(cur != NULL){
+        struct node *tmp = cur;
+        cur = cur->next;
+        free(tmp);
+    }
+    free(q);
+}
+
+struct node *newNode(int val){
+    struct node *n = malloc(sizeof(*n));
+    n->val = it;
+    n->next = NULL;
+    return n;
+}
+
+void QueueEnqueue (Queue q, int val){
+
+    struct node *n = newNode(it);
+    if(q->size == 0){
+        q->head = n;
+    } else {
+        q->tail->next = n;
+    }
+
+    q->tail = n;
+    q->size++;
+
+}
+
+int QueueDequeue(Queue q){
+
+    if(q->size == 0){
+        fprintf(stderr, "error: queue is empty\n");
+        return 0;
+    }
+
+    struct node *cur = q->head->next;
+    int val = q->head->val;
+
+    free(q->head);
+    q->head = cur;
+    if(cur == NULL){
+        q->tail = NULL;
+    }
+    q->size--;
+    
+    return val;
+}
+
+int QueueSize(Queue q){
+    return q->size;
+}
+
+int QueuePeek(Queue q){
+    if(q->size == 0){
+        fprintf(stderr, "error: queue is empty\n");
+        return 0;
+    }
+
+    return q->head->val;
+}
