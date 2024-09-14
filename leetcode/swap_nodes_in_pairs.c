b3050e1ec8749fa59aacc7436c62a0036c40e654
EzyCzy <82717285+EzyCzy@users.noreply.github.com>
2024-09-14 16:06:00 +1000

    Create swap_nodes_in_pairs.c

---
+++ leetcode/swap_nodes_in_pairs.c
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+void swapCall(struct ListNode* newHead){
+
+    if(newHead->next != NULL && newHead->next->next != NULL) swapCall(newHead->next->next);
+    else return;
+
+    struct ListNode *cur = newHead;
+    struct ListNode *k1 = newHead->next->next;
+    struct ListNode *k2 = k1->next;
+
+    struct ListNode *tmp = cur->next;
+
+    cur->next = k1;
+    k1->next = tmp;
+    tmp->next = k2;
+
+}
+
+struct ListNode* swapPairs(struct ListNode* head) {
+
+    struct ListNode *dummy = malloc(sizeof(*dummy));
+    dummy->val = 0;
+    dummy->next = head;
+
+    swapCall(dummy);
+    return dummy->next;
+}
